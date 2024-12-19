c33348f300fa2ac707c7b822a420af2ce62774f8
00xshashank <147302888+00xshashank@users.noreply.github.com>
2024-12-19 06:46:46 +0530

    Implemented AST and visitor pattern

---
+++ jlox/src/main/java/org/example/tool/GenerateAST.java
@@ -0,0 +1,89 @@
+package org.example.tool;
+
+import java.util.List;
+import java.util.Arrays;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+class GenerateAST {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: GenerateAST [dir]");
+            System.exit(64);
+        }
+        String outDir = args[0];
+        try {
+            defineAST(outDir, "Expr", Arrays.asList(
+                    "Binary   : Expr left, Token operator, Expr right",
+                    "Grouping : Expr expression",
+                    "Literal  : Object value",
+                    "Unary    : Token operator, Expr right"
+            ));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void defineAST(String outDir, String baseName, List<String> types) throws IOException {
+        String path = outDir + "/" + baseName + ".java";
+        PrintWriter pw = new PrintWriter(path, "UTF-8");
+
+        pw.println("package org.example.lox;");
+        pw.println();
+        pw.println("import java.util.List;");
+        pw.println();
+        pw.println("abstract class " + baseName  + " {");
+
+        defineVisitor(pw, baseName, types);
+
+        for (String type: types) {
+            String typeName = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(pw, baseName, typeName, fields);
+        }
+
+        pw.println("  abstract <R> R accept(Visitor<R> visitor);");
+
+        pw.println("}");
+        pw.close();
+    }
+
+    private static void defineType(PrintWriter pw, String baseName, String typeName, String fields) {
+        pw.println("\tstatic class " + typeName + " extends " + baseName + " {");
+        pw.println("\t\t" + typeName + " (" + fields + ") { ");
+
+        String[] fieldList = fields.split(", ");
+        for (String field: fieldList) {
+            String name = field.split(" ")[1].trim();
+            pw.println("\t\t\tthis." + name + " = " + name + ";");
+        }
+        pw.println("\t\t}");
+
+        pw.println();
+        for (String field: fieldList) {
+            String name = field.trim();
+            pw.println("\t\tfinal " + name + ";");
+        }
+
+        pw.println();
+        pw.println("\t\t@Override");
+        pw.println("\t\t<R> R accept (Visitor<R> visitor) {");
+        pw.println("\t\t\treturn visitor.visit" + typeName + baseName + "(this);");
+        pw.println("\t\t}");
+
+        pw.println("\t}");
+        pw.println();
+    }
+
+    private static void defineVisitor(PrintWriter pw, String baseName, List<String> types) {
+        pw.println("\tinterface Visitor<R> {");
+
+        for (String type: types) {
+            String typeName = type.split(":")[0].trim();
+            pw.println(
+                    "\t\tR visit" + typeName + baseName + " (" + typeName + " " + baseName.toLowerCase() + ");"
+            );
+        }
+        pw.println(("\t}"));
+    }
+}
