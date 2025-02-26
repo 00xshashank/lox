e5479ef7380061b435abea4cc76b7f187f55c134
00xshashank <147302888+00xshashank@users.noreply.github.com>
2025-02-26 08:24:47 +0530

    Added variable resolution pass

--- jlox/src/main/java/org/example/lox/Lox.java
+++ jlox/src/main/java/org/example/lox/Lox.java
@@ -61,6 +61,11 @@ class Lox {
 
         Parser parser = new Parser(tokens);
         List<Stmt> statements = parser.parse();
+
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+        if (hadError) return;
+
         interpreter.interpret(statements);
     }
 
